fae066f85c1bee7ab1291bbc58009a8637886b6f
Stacy-Sanders <stacy.sanders081@gmail.com>
2022-02-14 13:46:31 -0500

    added identity configurations

--- TechTrainingTracker.Data/IdentityModels.cs
+++ TechTrainingTracker.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -29,5 +31,40 @@ namespace TechTrainingTracker.Data
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Certification> Certifications { get; set; }
+        public DbSet<Training> Trainings { get; set; }
+        public DbSet<Skills> Skill { get; set; }
+        public DbSet<Companies> Company { get; set; }
+        public DbSet<Users> User { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                        .Conventions
+                        .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin> 
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
     }
 }
+
