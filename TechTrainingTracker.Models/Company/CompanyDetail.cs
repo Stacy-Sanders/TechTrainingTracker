d906edb73403e242e2edcc5b618441f5142fab2a
Stacy-Sanders <stacy.sanders081@gmail.com>
2022-02-16 14:49:48 -0500

    url route

--- TechTrainingTracker.Models/Company/CompanyDetail.cs
+++ TechTrainingTracker.Models/Company/CompanyDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,17 @@ namespace TechTrainingTracker.Models.Company
 {
     public class CompanyDetail
     {
+        public int CompanyID { get; set; }
 
+        [Required, Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Company has an app?")]
+        public bool HasApp { get; set; }
+
+        [Display(Name = "Has accredited courses?")]
+        public bool HasAccreditedCourses { get; set; }
+
+        public string Accreditation { get; set; }
     }
 }
