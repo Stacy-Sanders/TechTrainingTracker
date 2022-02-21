219fc29333f17deec6ec2e5e4b8c3c242925689b
Stacy-Sanders <stacy.sanders081@gmail.com>
2022-02-21 09:43:14 -0500

    seed data

--- TechTrainingTracker.Data/Migrations/Configuration.cs
+++ TechTrainingTracker.Data/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace TechTrainingTracker.Data.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TechTrainingTracker.Data;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TechTrainingTracker.Data.ApplicationDbContext>
     {
@@ -14,6 +15,11 @@ namespace TechTrainingTracker.Data.Migrations
 
         protected override void Seed(TechTrainingTracker.Data.ApplicationDbContext context)
         {
+            context.User.AddOrUpdate(u => u.UserID,
+                new User() { UserID = 1, FirstName = "Stacy", LastName = "Sanders", StreetAddress = "15078 NW Central Drive", StreetAddress2 = "Apt. 211", City = "Portland", State = "OR", ZipCode = "97229", PhoneNumber = "812-798-8818", EmailAddress = "stacy.sanders081@gmail.com" },
+                new User() { UserID =2, FirstName = "Sydney", LastName = "Donato", StreetAddress = "5065 W STATE ROAD 54", City = "Switz City", State = "IN", ZipCode = "47465", PhoneNumber = "812-798-9064", EmailAddress = "sydneydaisy@yahoo.com" }
+                );
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
